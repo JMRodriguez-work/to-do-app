75e20b6ada4609f3bc63a9702833870f9852169b
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2022-12-27 13:41:00 -0300

    Updates

---
+++ precache-manifest.9e0519e54d73d211242547a86b72a0fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b338f87bf73cfcd1fed9",
+    "url": "/to-do-app/static/css/main.3518377c.chunk.css"
+  },
+  {
+    "revision": "b338f87bf73cfcd1fed9",
+    "url": "/to-do-app/static/js/main.b338f87b.chunk.js"
+  },
+  {
+    "revision": "480a353721bd092f9455",
+    "url": "/to-do-app/static/js/1.480a3537.chunk.js"
+  },
+  {
+    "revision": "60e6816a297c28fee7c8",
+    "url": "/to-do-app/static/js/runtime~main.60e6816a.js"
+  },
+  {
+    "revision": "f9b1eccd8b11aeb1aeb990f0c9901c27",
+    "url": "/to-do-app/index.html"
+  }
+];
